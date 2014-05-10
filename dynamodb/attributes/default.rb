a3bf56a7542f65967d4813b66b947861d9be8df9
eric-wu <eric.wu@sagebase.org>
2014-05-09 18:17:55 -0700

    Fix Chef compile errors.

--- dynamodb/attributes/default.rb
+++ dynamodb/attributes/default.rb
@@ -1,6 +1,6 @@
-default[:dynamodb][:local][:java][:pkg] = 'java-1.7.0-openjdk.x86_64'
-default[:dynamodb][:local][:pkg] = 'dynamodb_local_latest'
-default[:dynamodb][:local][:pkg][:url] = 'http://dynamodb-local.s3-website-us-west-2.amazonaws.com/#{node[:dynamodb][:local][:pkg]}'
-default[:dynamodb][:local][:dir] = 'dynamodb_local'
+default[:dynamodb][:local][:java][:pkg] = "java-1.7.0-openjdk.x86_64"
+default[:dynamodb][:local][:pkg] = "dynamodb_local_latest"
+default[:dynamodb][:local][:pkg][:url] = "http://dynamodb-local.s3-website-us-west-2.amazonaws.com/#{node[:dynamodb][:local][:pkg]}"
+default[:dynamodb][:local][:dir] = "/opt/dynamodb_local"
 default[:dynamodb][:local][:port] = 8001
-default[:dynamodb][:local][:java_opts] = '-inMemory -port #{node[:dynamodb][:local][:port]}'
+default[:dynamodb][:local][:java_opts] = "-inMemory -port #{node[:dynamodb][:local][:port]}"
--- dynamodb/recipes/setup.rb
+++ dynamodb/recipes/setup.rb
@@ -1,4 +1,4 @@
-packages = ['#{node[:dynamodb][:local][:java][:pkg]}', 'wget']
+packages = ["#{node[:dynamodb][:local][:java][:pkg]}"]
 
 packages.each do |pkg|
   package pkg do
@@ -6,21 +6,31 @@ packages.each do |pkg|
   end
 end
 
-bash 'install dynamodb local' do
+directory "#{node[:dynamodb][:local][:dir]}" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
+
+remote_file "#{node[:dynamodb][:local][:dir]}/#{node[:dynamodb][:local][:pkg]}" do
+  source "#{node[:dynamodb][:local][:pkg][:url]}"
+end
+
+bash "install dynamodb local" do
+  user "root"
   code <<-EOC
-    cd ~
-    mkdir #{node[:dynamodb][:local][:dir]}
     cd #{node[:dynamodb][:local][:dir]}
-    wget #{node[:dynamodb][:local][:pkg][:url]}
     tar xzf #{node[:dynamodb][:local][:pkg]}
-    rm #{node[:dynamodb][:local][:pkg]}
-    cd ~
   EOC
 end
 
-execute 'start dynamodb local' do
-  command 'cd ~/#{node[:dynamodb][:local][:dir]} &&
-           java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar #{node[:dynamodb][:local][:java_opts]} & &&
-           cd ~'
+file "#{node[:dynamodb][:local][:dir]}/#{node[:dynamodb][:local][:pkg]}" do
+  action :delete
+end
+
+execute "start dynamodb local" do
+  command "cd ~/#{node[:dynamodb][:local][:dir]} &&
+           java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar #{node[:dynamodb][:local][:java_opts]} &"
   action :run
 end
